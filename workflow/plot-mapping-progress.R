d6ac5a13ed1f5cfe4cc3f3bf165080fb1b615843
Michael Koontz <mikoontz@gmail.com>
2021-06-05 18:18:23 -0600

    sampling day as a factor for better visualization

--- workflow/plot-mapping-progress.R
+++ workflow/plot-mapping-progress.R
@@ -10,20 +10,19 @@ library(lubridate)
 dir.create("figs", showWarnings = FALSE)
 
 cw <- sf::st_read("data/out/calwood-property-boundary.gpkg")
-cw_fire <- sf::st_read("data/out/calwood-fire-perimeter.gpkg") %>% slice(2)
+cw_fire <- sf::st_read("data/out/calwood-fire-perimeter.gpkg")
 
 metadata <- 
-  list.files("data/drone/L0/metadata", full.names = TRUE) %>% 
+  list.files("data/drone/L0/metadata-lite/", full.names = TRUE) %>% 
   pblapply(FUN = fread) %>% 
   data.table::rbindlist()
 
 photo_points <- 
   metadata %>% 
-  dplyr::select(FileName, DateTimeOriginal, GPSLongitude, GPSLatitude) %>% 
   st_as_sf(coords = c("GPSLongitude", "GPSLatitude"), crs = 4326) %>% 
   sf::st_transform(sf::st_crs(cw)) %>% 
   dplyr::mutate(DateTimeOriginal = lubridate::ymd_hms(DateTimeOriginal),
-                sampling_day = 1 + as.numeric(difftime(trunc(DateTimeOriginal, units = "days"), min(trunc(DateTimeOriginal, units = "days")), units = "days")))
+                sampling_day = as.factor(1 + as.numeric(difftime(trunc(DateTimeOriginal, units = "days"), min(trunc(DateTimeOriginal, units = "days")), units = "days"))))
 
 photo_points
 
@@ -31,7 +30,7 @@ sampling_progress_gg <-
   ggplot() +
   geom_sf(data = cw, alpha = 0.5, fill = "blue") +
   geom_sf(data = cw_fire, color = "red", lwd = 2, fill = NA) +
-  geom_sf(data = photo_points, aes(color = sampling_day), alpha = 1, cex = 0.75) +
+  geom_sf(data = photo_points, aes(color = sampling_day)) +
   theme_bw() +
   coord_sf(xlim = c(st_bbox(cw)[c(1, 3)]), ylim = c(st_bbox(cw)[c(2, 4)]))
 
